4eca5eb3961a4435a6892e6bcbc96d3cb4b6d8f1
shaw <a1327793405@qq.com>
2016-11-20 20:45:12 +0800

    proj1

---
+++ PersistenceLayer/PersistenceLayer/PersistenceLayer.h
@@ -0,0 +1,19 @@
+//
+//  PersistenceLayer.h
+//  PersistenceLayer
+//
+//  Created by Kardel on 16/11/20.
+//  Copyright © 2016年 Kardel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PersistenceLayer.
+FOUNDATION_EXPORT double PersistenceLayerVersionNumber;
+
+//! Project version string for PersistenceLayer.
+FOUNDATION_EXPORT const unsigned char PersistenceLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PersistenceLayer/PublicHeader.h>
+
+
---
+++ PresentationLayer/PresentationLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PresentationLayer
+//
+//  Created by Kardel on 16/11/20.
+//  Copyright © 2016年 Kardel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
